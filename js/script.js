3c93a15743e23c24bfa5b9f4f399aa9c2cab682e
mrudulkolambe <mrudulkolambe02@gmail.com>
2022-01-07 01:08:41 +0530

    Changes

--- js/script.js
+++ js/script.js
@@ -7,13 +7,13 @@ document.querySelectorAll('.nav-links').forEach(anchor => {
     })
 })
 
-setTimeout(() => {
-    document.querySelector(".loading").classList.add('shutLoading');
-    document.querySelector("body").style.overflowY = "scroll";
-}, 3000);
-setTimeout(() => {
-    document.querySelector(".loading").style.display = "none";
-}, 3700);
+// setTimeout(() => {
+//     document.querySelector(".loading").classList.add('shutLoading');
+//     document.querySelector("body").style.overflowY = "scroll";
+// }, 3000);
+// setTimeout(() => {
+//     document.querySelector(".loading").style.display = "none";
+// }, 3700);
 
 let hamburger = document.querySelector('.hamburger');
 let mobile = document.querySelector(".mobile");
